db980671afa14c18f33bd0248b5571119a520ffb
Hakan ÖZER <hakanozer02@gmail.com>
2022-08-02 11:57:44 +0300

    NavBar

--- ServletApp/src/main/java/services/LoginService.java
+++ ServletApp/src/main/java/services/LoginService.java
@@ -2,18 +2,37 @@ package services;
 
 import utils.DB;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class LoginService {
 
     // DB...
 
     public boolean login( String email, String password ) {
         DB db = new DB();
-        db.getConn();
-        if ( email.equals("ali@mail.com") && password.equals("12345") ) {
-            return  true;
+        boolean status = false;
+        try {
+            String sql = "select * from admin where email = ? and password = ?";
+            //String sql = " select * from admin where email = '"+email+"' and password = '"+password+"' ";
+            // select * from admin where email = 'a@a.com' and password = '' or 1 = 1 --'
+            // Statement st = db.getConn().createStatement();
+            PreparedStatement st = db.getConn().prepareStatement(sql);
+            st.setString(1, email);
+            st.setString(2, password);
+
+            ResultSet rs = st.executeQuery();
+            status = rs.next();
+            if ( status ) {
+                System.out.println("Giriş Başarılı");
+            }else {
+                System.out.println("Kullanıcı bilgileri hatalı!");
+            }
+        }catch (Exception ex) {
+            System.err.println("login Error : " + ex);
         }
-        db.close();
-        return false;
+        return status;
     }
 
 }
